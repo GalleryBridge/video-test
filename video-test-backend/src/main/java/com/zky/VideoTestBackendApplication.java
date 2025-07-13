f0da7b02c1f11a957febd7e51e4195d602965db9
Laidor <laidoring@163.com>
2025-07-13 20:14:35 +0800

    init

---
+++ video-test-backend/src/main/java/com/zky/VideoTestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoTestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoTestBackendApplication.class, args);
+    }
+
+}
---
+++ video-test-backend/src/test/java/com/zky/VideoTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoTestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
